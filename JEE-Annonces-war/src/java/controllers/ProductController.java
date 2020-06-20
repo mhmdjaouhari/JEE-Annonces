0281eabae53169c67fce238412c16a43f8c5b57b
mojaouhari <jaouhari1998@gmail.com>
2020-06-20 16:00:08 +0100

    Add images upload

--- JEE-Annonces-war/src/java/controllers/ProductController.java
+++ JEE-Annonces-war/src/java/controllers/ProductController.java
@@ -6,16 +6,28 @@
 package controllers;
 
 import entities.Category;
+import entities.Image;
 import entities.Product;
 import entities.User;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.time.Instant;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.context.FacesContext;
-import javax.faces.view.ViewScoped;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import models.ImageFacade;
 import models.ProductFacade;
 
 /**
@@ -56,7 +68,12 @@ public class ProductController implements Serializable {
     @EJB
     private ProductFacade productFacade;
 
+    @EJB
+    private ImageFacade imageFacade;
+
     private Product product;
+    private Part file;
+    private HashSet<String> imageFileNames = new HashSet();
 
     public Product getProduct() {
         return product;
@@ -64,6 +81,14 @@ public class ProductController implements Serializable {
 
     public void setProduct(Product product) {
         this.product = product;
+    }
+
+    public Part getFile() {
+        return null; // important to allow upload of multiple files
+    }
+
+    public void setFile(Part file) {
+        this.file = file;
     }
 
     public List<Product> findAll() {
@@ -79,8 +104,17 @@ public class ProductController implements Serializable {
     }
 
     public String insert() {
+        // insert product
         productFacade.create(product);
         int id = product.getId();
+        // insert product images
+        for (String filePath : imageFileNames) {
+            Image image = new Image();
+            image.setUrl(filePath);
+            image.setProductId(product);
+            imageFacade.create(image);
+        }
+        System.out.println("DONE INSERT");
         return "/product?faces-redirect=true&id=" + id;
     }
 
@@ -89,9 +123,49 @@ public class ProductController implements Serializable {
         int id = product.getId();
         return "/product?faces-redirect=true&id=" + id;
     }
-    
-    public String delete(Product product){
+
+    public String delete(Product product) {
+        for (Image image : product.getImageList()) {
+            // remove image from db
+            imageFacade.remove(image);
+            // remove image file
+
+        }
+        // remove product
         productFacade.remove(product);
-        return "index?faces-redirect=true";
+        return "/index?faces-redirect=true";
     }
+
+    public static Collection<Part> getAllParts(Part part) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        return request.getParts().stream().filter(p -> part.getName().equals(p.getName())).collect(Collectors.toList());
+    }
+
+    public void uploadImages() {
+        try {
+            for (Part file : getAllParts(file)) {
+                // upload the file
+                InputStream in = file.getInputStream();
+                String fileName = file.getSubmittedFileName() + "-" + Instant.now().getEpochSecond();
+                File f = new File("/home/jaouhari/NetBeansProjects/JEE-Annonces/JEE-Annonces-war/web/resources/images/uploaded/"
+                        + fileName);
+                f.createNewFile();
+                FileOutputStream out = new FileOutputStream(f);
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = in.read(buffer)) > 0) {
+                    out.write(buffer, 0, length);
+                }
+                out.close();
+                in.close();
+                // memorize filenames (unique)
+                imageFileNames.add(fileName);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+
+    }
+
 }
